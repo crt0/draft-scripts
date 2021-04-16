7b8e7d29ea9825a607f1ad4659962c42e52135ef
Andrew Korty <andrew.korty@icloud.com>
2021-04-15 22:06:37 -0400

    Cancel selection after inserting date

--- Insert Short Date.js
+++ Insert Short Date.js
@@ -3,6 +3,12 @@
 // (Of the format Tue, 8/18)
 
 (() => { // anonymous function prevents variable conflicts with other Drafts actions
-	let date_options = { weekday: 'short', month: 'numeric', day: 'numeric' };
-	editor.setSelectedText((new Date()).toLocaleDateString("en-us", date_options));
+	const date_options = { weekday: 'short', month: 'numeric', day: 'numeric' };
+	
+	// replace selection with short date
+	editor.setSelectedText((new Date()).toLocaleDateString('en-us', date_options));
+	
+	// cancel selection
+	const range = editor.getSelectedRange();
+	editor.setSelectedRange(range[0] + range[1], 0);
 })();
