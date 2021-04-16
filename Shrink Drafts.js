26135a636df3b7b0ca5ed5d6d0f88a68cd396f05
Andrew Korty <andrew.korty@icloud.com>
2021-04-15 21:44:17 -0400

    Hide the action and draft lists

---
+++ Shrink Drafts.js
@@ -0,0 +1,8 @@
+// Shrink Drafts for notetaking
+//
+// 
+
+(() => { // anonymous function prevents variable conflicts with other Drafts actions
+	app.hideActionList();
+	app.hideDraftList();
+})();
