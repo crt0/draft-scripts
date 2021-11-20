cd1e90c4bb8addebba4076553dcd7007813cfc3b
Andrew Korty <andrew.korty@icloud.com>
2021-11-20 18:41:38 -0500

    Standardize indentation

--- Switch to Workspace.js
+++ Switch to Workspace.js
@@ -1,9 +1,9 @@
 (() => {
-	let workspace_name = action.name.split(' ', 2)[0];
-	let workspace = Workspace.find(workspace_name);
-	if (!workspace) {
-		context.fail("Can't find workspace " + workspace_name);
-		return;
-	}
-	app.applyWorkspace(workspace);
+    let workspace_name = action.name.split(' ', 2)[0];
+    let workspace = Workspace.find(workspace_name);
+    if (!workspace) {
+        context.fail("Can't find workspace " + workspace_name);
+        return;
+    }
+    app.applyWorkspace(workspace);
 })();
