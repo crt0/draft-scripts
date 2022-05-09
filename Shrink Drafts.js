2accd35b575bcc1878213072883b8caaa12c0e3e
Andrew Korty <andrew.korty@icloud.com>
2022-05-09 13:01:41 -0400

    Replace scripts with built-in Drafts functionality

--- Shrink Drafts.js
+++
@@ -1,8 +0,0 @@
-// Shrink Drafts for notetaking
-//
-// 
-
-(() => { // anonymous function prevents variable conflicts with other Drafts actions
-	app.hideActionList();
-	app.hideDraftList();
-})();
--- Switch to Workspace.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    let workspace_name = action.name.split(' ', 2)[0];
-    let workspace = Workspace.find(workspace_name);
-    if (!workspace) {
-        context.fail("Can't find workspace " + workspace_name);
-        return;
-    }
-    app.applyWorkspace(workspace);
-})();
