cab590d5e9fa21ae1dc226483cc260f55dfda9da
Andrew Korty <andrew.korty@icloud.com>
2022-06-10 13:23:01 -0400

    Shorten variable names for easier reading

--- New Brewday Entry.js
+++ New Brewday Entry.js
@@ -14,10 +14,10 @@
     const brewday_drafts = workspace.query('all');
 
     // prompt to select
-    let prompt = Prompt.create();
-    prompt.title = 'New Brewday Entry';
-    prompt.message = 'Select draft. A new entry will be created in that '
-      + 'draft. If no draft is selected, a new draft will be created.';
+    let p = Prompt.create();
+    p.title = 'New Brewday Entry';
+    p.message = 'Select draft. A new entry will be created in that draft. '
+      + 'If no draft is selected, a new draft will be created.';
 
     let ix = 0;
     let identifiers = [];
@@ -25,12 +25,12 @@
         identifiers[ix] = ix + '. ' + d.displayTitle;
         ix++;
     }
-    prompt.addSelect('beers', 'Beers', identifiers, []);
-    prompt.addTextField('new_beer', 'New Beer', '');
-    prompt.addDatePicker('brewdate', 'Brew Date', new Date(), {'mode': 'date'})
-    prompt.addButton('OK');
+    p.addPicker('beers', 'Beers', [brewday_drafts.map(d => d.displayTitle)]);
+    p.addTextField('new_beer', 'New Beer', '');
+    p.addDatePicker('brewdate', 'Brew Date', new Date(), {mode: 'date'})
+    p.addButton('OK');
 
-    const ok_clicked = prompt.show();
+    const ok_clicked = p.show();
     if (!ok_clicked) {
         context.cancel();
         return;
@@ -44,22 +44,26 @@
                      + '"_brewday_entry" and "template"');
         return;
     }
-    const template = template_drafts[0].processTemplate(template_drafts[0]
-                                                        .content);
+    let td = template_drafts[0];
+    td.setTemplateTag('brewdate', p.fieldValues
+                                   .brewdate
+                                   .toLocaleString(undefined,
+                                                   {dateStyle: 'full'}));
+    const section = td.processTemplate(td.content);
 
     // create a new draft or use the selected one
     let beer_draft;
-    if (prompt.fieldValues.new_beer) {
+    if (p.fieldValues.new_beer) {
         beer_draft = Draft.create();
-        beer_draft.content = '# ' + prompt.fieldValues.new_beer.trim()
-          + '\n\n' + template;
+        beer_draft.content = '# ' + p.fieldValues.new_beer.trim() + '\n\n'
+          + section;
         beer_draft.addTag('brewday');
         beer_draft.addTag('brewing');
         beer_draft.update();
         editor.load(beer_draft);
     }
     else {
-        const selected_beers = prompt.fieldValues['beers'];
+        const selected_beers = p.fieldValues['beers'];
         if (selected_beers.length < 1) {
             context.fail('Must choose a draft or specify a name for a new '
                          + 'one');
