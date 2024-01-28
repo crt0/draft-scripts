f900141ed729e44d5a305470be93c826bbb1cd78
Andrew Korty <andrew.korty@icloud.com>
2024-01-28 04:57:33 -0500

    Implement a parser that takes the note as input and filters it so as not to pass any empty Markdown sections or list items that begin with `* ` (so those can be notes to myself). It also changes any tri-state tasks (`- { } `) to “Andrew will” statements.

--- Meeting Notes Mail Draft.js
+++ Meeting Notes Mail Draft.js
@@ -1,6 +1,58 @@
 (() => { // anonymous function prevents variable conflicts with other actions
+    const ME = 'Andrew';
 
     const recip_drafts = Draft.query('Mail Notes Recipients', 'archive',
+    function process_markdown_section(input, index) {
+        let nonempty = 0;
+        let level = '';
+
+        let head_re        = new RegExp('^(#+)\\s+');
+        let nonempty_re    = new RegExp('^[^\\s]+');
+        let hidden_item_re = new RegExp('^\\s*\\*\\s');
+        let task_re        = new RegExp('^(\\s*[-+] ){[- x]}\\s+([^\\s])');
+
+        let matches = input[index].match(head_re);
+        if (matches)
+            level = matches[1];
+        let output = [input[index]];
+
+        for (index++; index < input.length; index++) {
+            let nested_re = new RegExp(`^(${level}#+)\\s+`);
+            let line = input[index];
+
+            matches = line.match(nested_re);
+            if (matches) {
+                let result = process_markdown_section(input, index);
+                if (result.output.length) {
+                    output.push(...result.output);
+                    nonempty = 1;
+                }
+                index = result.index - 1;
+                continue;
+            }
+
+            matches = line.match(head_re);
+            if (matches)
+                if (nonempty)
+                    return {output: output, index: index};
+                else
+                    return {output: [], index: index};
+
+            if (line.match(hidden_item_re))
+                continue;
+
+            if (line.match(nonempty_re))
+                nonempty = 1;
+
+            line = line.replace(task_re, `$1${ME} will `
+                                + "$2".toLowerCase());
+
+            output.push(line);
+        }
+
+        return {output: output, index: index};
+    }
+
                                     ['_etc']);
     let name = '';
     let email = '';
@@ -33,9 +85,15 @@
           '. Thanks for taking the time to meet. Here are my notes.\n';
     }
 
+
+
     const found = draft.content.match(/\n(.*)/s);
     if (found) {
-        body += found[1]; //.replace(/^(#+)/gm, '$1#'); // shrink headings
+        let {output} = process_markdown_section(found[1].split('\n'), 0);
+        if (output)
+            body += output.join('\n');
+        else
+            body += found[1];
     }
 
     if (email) {
